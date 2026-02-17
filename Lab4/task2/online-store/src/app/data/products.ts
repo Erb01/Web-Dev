3dbda4aaaae9959e9fbcd2d61c1e5244649a3ddb
Yerbol <tald1774@gmail.com>
2026-02-18 00:36:59 +0500

    lab4

---
+++ Lab4/task2/online-store/src/app/data/products.ts
@@ -0,0 +1,146 @@
+import { Product } from '../models/product.model';
+
+export const PRODUCTS: Product[] = [
+  {
+    id: 1,
+    name: 'iPhone 13',
+    description: 'Powerful smartphone with A15 chip and great camera.',
+    price: 350000,
+    rating: 4.8,
+    image: 'images/Iphone13.jpg',
+    images: [
+      'images/Iphone13.jpg',
+      'https://via.placeholder.com/201',
+      'https://via.placeholder.com/202'
+    ],
+    link: 'https://kaspi.kz'
+  },
+  {
+    id: 2,
+    name: 'Samsung TV',
+    description: 'Smart TV with 4K resolution and HDR support.',
+    price: 280000,
+    rating: 4.6,
+    image: 'images/samsung.jpg',
+    images: [
+      'https://via.placeholder.com/200',
+      'https://via.placeholder.com/201',
+      'https://via.placeholder.com/202'
+    ],
+    link: 'https://kaspi.kz'
+  },
+  {
+  id: 3,
+  name: 'Xiaomi Redmi Note 12',
+  description: 'Affordable smartphone with AMOLED display and strong battery.',
+  price: 120000,
+  rating: 4.5,
+  image: 'images/xiaomi.jpg',
+  images: [
+    'https://resources.cdn-kaspi.kz/img/m/p/hc1/h5d/69545200300062.jpg',
+    'https://resources.cdn-kaspi.kz/img/m/p/h12/h65/69545200431134.jpg',
+    'https://resources.cdn-kaspi.kz/img/m/p/h65/hfe/69545200562206.jpg'
+  ],
+  link: 'https://kaspi.kz/shop/p/xiaomi-redmi-note-12-128gb-109905799/'
+},
+{
+  id: 4,
+  name: 'AirPods Pro',
+  description: 'Wireless earbuds with noise cancellation.',
+  price: 95000,
+  rating: 4.7,
+  image: 'images/airpods.jpg',
+  images: [
+    'https://resources.cdn-kaspi.kz/img/m/p/h1e/h7c/64082972758046.jpg',
+    'https://resources.cdn-kaspi.kz/img/m/p/h89/h4e/64082973118494.jpg',
+    'https://resources.cdn-kaspi.kz/img/m/p/h45/h93/64082973544478.jpg'
+  ],
+  link: 'https://kaspi.kz/shop/p/apple-airpods-pro-102102298/'
+},
+{
+  id: 5,
+  name: 'HP Pavilion Laptop',
+  description: 'Powerful laptop for study and work.',
+  price: 420000,
+  rating: 4.6,
+  image: 'images/hp.jpg',
+  images: [
+    'https://resources.cdn-kaspi.kz/img/m/p/h6b/hb7/64123456789022.jpg',
+    'https://resources.cdn-kaspi.kz/img/m/p/h12/h22/64123456789111.jpg',
+    'https://resources.cdn-kaspi.kz/img/m/p/h98/h33/64123456789222.jpg'
+  ],
+  link: 'https://kaspi.kz/shop/p/hp-pavilion-15-123456/'
+},
+{
+  id: 6,
+  name: 'PlayStation 5',
+  description: 'Next-gen console with ultra fast SSD.',
+  price: 380000,
+  rating: 4.9,
+  image: 'images/ps5.jpg',
+  images: [
+    'https://resources.cdn-kaspi.kz/img/m/p/hdd/h01/64335555555550.jpg',
+    'https://resources.cdn-kaspi.kz/img/m/p/hdd/h02/64335555555551.jpg',
+    'https://resources.cdn-kaspi.kz/img/m/p/hdd/h03/64335555555552.jpg'
+  ],
+  link: 'https://kaspi.kz/shop/p/sony-playstation-5-100746577/'
+},
+{
+  id: 7,
+  name: 'Apple Watch Series 8',
+  description: 'Smartwatch with health monitoring.',
+  price: 210000,
+  rating: 4.8,
+  image: 'images/applewatch.jpg',
+  images: [
+    'https://resources.cdn-kaspi.kz/img/m/p/h55/h88/64555555555000.jpg',
+    'https://resources.cdn-kaspi.kz/img/m/p/h55/h89/64555555555001.jpg',
+    'https://resources.cdn-kaspi.kz/img/m/p/h55/h90/64555555555002.jpg'
+  ],
+  link: 'https://kaspi.kz/shop/p/apple-watch-series-8-104777777/'
+},
+{
+  id: 8,
+  name: 'Logitech Mouse',
+  description: 'Ergonomic wireless mouse for everyday use.',
+  price: 15000,
+  rating: 4.4,
+  image: 'images/logitech.jpg',
+  images: [
+    'https://resources.cdn-kaspi.kz/img/m/p/h66/h12/64666666666000.jpg',
+    'https://resources.cdn-kaspi.kz/img/m/p/h66/h13/64666666666001.jpg',
+    'https://resources.cdn-kaspi.kz/img/m/p/h66/h14/64666666666002.jpg'
+  ],
+  link: 'https://kaspi.kz/shop/p/logitech-mouse-910111/'
+},
+{
+  id: 9,
+  name: 'Samsung Galaxy Tab',
+  description: 'Tablet for entertainment and productivity.',
+  price: 260000,
+  rating: 4.6,
+  image: 'images/samsungtab.jpg',
+  images: [
+    'https://resources.cdn-kaspi.kz/img/m/p/h77/h11/64777777777000.jpg',
+    'https://resources.cdn-kaspi.kz/img/m/p/h77/h12/64777777777001.jpg',
+    'https://resources.cdn-kaspi.kz/img/m/p/h77/h13/64777777777002.jpg'
+  ],
+  link: 'https://kaspi.kz/shop/p/samsung-galaxy-tab-102222/'
+},
+{
+  id: 10,
+  name: 'JBL Speaker',
+  description: 'Portable Bluetooth speaker with deep bass.',
+  price: 55000,
+  rating: 4.7,
+  image: 'images/jbl.jpg',
+  images: [
+    'https://resources.cdn-kaspi.kz/img/m/p/h88/h21/64888888888000.jpg',
+    'https://resources.cdn-kaspi.kz/img/m/p/h88/h22/64888888888001.jpg',
+    'https://resources.cdn-kaspi.kz/img/m/p/h88/h23/64888888888002.jpg'
+  ],
+  link: 'https://kaspi.kz/shop/p/jbl-speaker-777888/'
+}
+
+
+];
